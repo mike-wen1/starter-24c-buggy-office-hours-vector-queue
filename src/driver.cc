d4f95719cadcfac1bfffff15ca59431de10bf951
mike-wen1 <torchmaster123@gmail.com>
2025-02-24 04:39:04 +0000

    Full score

--- src/driver.cc
+++ src/driver.cc
@@ -5,6 +5,8 @@
 #include "staff.hpp"
 #include "student.hpp"
 
+using namespace std;
+
 int main() {
   OfficeHoursQueue queue;
 
--- src/functions.cc
+++ src/functions.cc
@@ -3,16 +3,48 @@
 #include <iostream>
 #include <random>
 
+using namespace std;
+
 constexpr int kRandomMin = 5;
 constexpr int kRandomMax = 20;
 
 void AddStudent(OfficeHoursQueue& queue, const Student& student) {
-  queue.student_queue.push_back(student);
-  queue.student_queue.back().arrival_order = queue.student_arrival_counter;
+  // Update arrival order
+  int new_student_arrival_order = queue.student_arrival_counter + 1;
+
+  // Go through and sort by attendence, then arrival order
+  bool at_end = true;
+  for (int i = 0; i < (int)queue.student_queue.size(); i++) {
+    if (student.attendance_percentage >
+        queue.student_queue[i]
+            .attendance_percentage) {  // Should account for arrival order
+      queue.student_queue.push_back(
+          queue.student_queue[(int)queue.student_queue.size() - 1]);
+      for (int j = (int)queue.student_queue.size() - 1; j > i; j--) {
+        queue.student_queue[j] = queue.student_queue[j - 1];
+      }
+      queue.student_queue[i] = Student{student.name,
+                                       student.student_id,
+                                       student.attendance_percentage,
+                                       new_student_arrival_order};
+
+      at_end = false;
+      break;
+    }
+  }
+
+  if (at_end) {
+    queue.student_queue.push_back(Student{student.name,
+                                          student.student_id,
+                                          student.attendance_percentage,
+                                          new_student_arrival_order});
+  }
+
   queue.student_arrival_counter += 1;
 }
 
 void AddStaff(OfficeHoursQueue& queue, const Staff& staff) {
+  /*
   queue.staff_queue.push_back(staff);
   queue.staff_queue.back().arrival_order = queue.staff_arrival_counter;
   queue.staff_arrival_counter += 1;
@@ -40,6 +72,43 @@ void AddStaff(OfficeHoursQueue& queue, const Staff& staff) {
       break;
     }
   }
+  */
+
+  // Update arrival order
+  int new_staff_arrival_order = queue.staff_arrival_counter + 1;
+
+  // Go through, sort by fewer "encounters" (students helped), then by less
+  // total help time, then arrival order Again, looping from the start to the
+  // end of the queue should sort by arrival order
+  bool at_end = true;
+  for (int i = 0; i < (int)queue.staff_queue.size(); i++) {
+    if ((staff.encounter_count < queue.staff_queue[i].encounter_count) ||
+        (staff.encounter_count == queue.staff_queue[i].encounter_count &&
+         staff.total_help_time < queue.staff_queue[i].total_help_time)) {
+      queue.staff_queue.push_back(
+          queue.staff_queue[(int)queue.staff_queue.size() - 1]);
+      for (int j = (int)queue.staff_queue.size() - 1; j > i; j--) {
+        queue.staff_queue[j] = queue.staff_queue[j - 1];
+      }
+      queue.staff_queue[i] = Staff{staff.name,
+                                   staff.staff_id,
+                                   staff.encounter_count,
+                                   new_staff_arrival_order,
+                                   staff.total_help_time};
+
+      at_end = false;
+      break;
+    }
+  }
+  if (at_end) {
+    queue.staff_queue.push_back(Staff{staff.name,
+                                      staff.staff_id,
+                                      staff.encounter_count,
+                                      new_staff_arrival_order,
+                                      staff.total_help_time});
+  }
+
+  queue.staff_arrival_counter += 1;
 }
 
 void HelpNextStudent(OfficeHoursQueue& queue) {
@@ -54,6 +123,8 @@ void HelpNextStudent(OfficeHoursQueue& queue) {
 
   Student student = queue.student_queue[0];
   queue.student_queue.erase(queue.student_queue.begin());
+  Staff staff = queue.staff_queue[0];
+  queue.staff_queue.erase(queue.staff_queue.begin());
 
   // Generate random help time between kRandomMin and kRandomMax minutes
   static std::random_device rd;
@@ -61,7 +132,6 @@ void HelpNextStudent(OfficeHoursQueue& queue) {
   std::uniform_int_distribution<> dist(kRandomMin, kRandomMax);
   int help_time = dist(gen);
 
-  Staff staff = queue.staff_queue[queue.staff_queue.size() - 1];
   staff.encounter_count++;
   staff.total_help_time += help_time;
   AddStaff(queue, staff);
